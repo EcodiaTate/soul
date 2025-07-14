194a918dc67f64c7537081793cc1a93489331c99
EcodiaTate <tate@ecodia.au>
2025-07-15 08:29:42 +1000

    Update

--- core/graph_io.py
+++ core/graph_io.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 NEO4J_URI = os.environ.get("NEO4J_URI", "bolt://localhost:7687")
 NEO4J_USER = os.environ.get("NEO4J_USER", "neo4j")
-NEO4J_PASSWORD = os.environ.get("NEO4J_PASSWORD", "password")
+NEO4J_PASSWORD = os.environ.get("NEO4J_PASS", "password")
 
 driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
 def vector_search(embedding, top_k=8):

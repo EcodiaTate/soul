e69e37492de1e244c6584c4380740fac6cf0ff33
EcodiaTate <tate@ecodia.au>
2025-07-17 12:27:51 +1000

    Update

--- config/settings.py
+++ config/settings.py
@@ -3,12 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Neo4j
-NEO4J_URI = os.getenv("NEO4J_URI")
-NEO4J_USER = os.getenv("NEO4J_USER")
-NEO4J_PASS = os.getenv("NEO4J_PASS")
-
-# OpenAI
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
-# Any other keys go here...
+def load_config():
+    """Return all relevant config as a dict."""
+    return {
+        "NEO4J_URI": os.getenv("NEO4J_URI"),
+        "NEO4J_USER": os.getenv("NEO4J_USER"),
+        "NEO4J_PASS": os.getenv("NEO4J_PASS"),
+        "OPENAI_API_KEY": os.getenv("OPENAI_API_KEY"),
+        "JWT_SECRET": os.getenv("JWT_SECRET"),
+        # ...add more as needed
+    }
